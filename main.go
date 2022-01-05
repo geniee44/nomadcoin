24b6dd2be243d376ae35f4a3cb5b3cf8f7be6e65
int-gen <genie@crossangle.io>
2022-01-05 19:24:25 +0900

    learn how to use array

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func plus(a ...int) int {
-    var total int
-    for _, item := range a{
-        total += item
-    }
-    return total
+	var total int
+	for _, item := range a {
+		total += item
+	}
+	return total
 }
 func main() {
 	result := plus(2, 3, 4, 5)
 	fmt.Println(result)
+
+	//array
+	foods := [3]string{"potato", "pizza", "pasta"}
+
+	for _, food := range foods {
+		fmt.Println(food)
+	}
+
+	//위와 동일하지만 loop 방식으로
+	for i := 0; i < len(foods); i++ {
+		fmt.Println(foods[i])
+	}
 }
