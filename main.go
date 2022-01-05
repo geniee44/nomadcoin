f085f62934a807ec781975727745c41757e56675
geniee44 <geeniee44@gmail.com>
2022-01-05 23:50:47 +0900

    learn slice

--- main.go
+++ main.go
@@ -18,12 +18,20 @@ func main() {
 	//array
 	foods := [3]string{"potato", "pizza", "pasta"}
 
-	for _, food := range foods {
-		fmt.Println(food)
-	}
+	// for _, food := range foods {
+	// 	fmt.Println(food)
+	// }
 
 	//위와 동일하지만 loop 방식으로
 	for i := 0; i < len(foods); i++ {
 		fmt.Println(foods[i])
+	}
+
+	fmt.Println("\nslices ver.")
+	//slice <- array는 길이를 미리 명시해둬야 하기 때문에 블록체인 같이 길이를 예측할 수 없는 경우 slice 사용
+	dishes := []string{"potato", "pizza", "pasta"}
+
+	for i := 0; i < len(dishes); i++ {
+		fmt.Println(dishes[i])
 	}
 }
