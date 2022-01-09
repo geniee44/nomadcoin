5486d6d67d95619c6ac9ffef64d6d82e6b22bb41
geniee44 <geeniee44@gmail.com>
2022-01-09 16:41:00 +0900

    add struct

--- main.go
+++ main.go
@@ -11,27 +11,40 @@ func plus(a ...int) int {
 	}
 	return total
 }
-func main() {
-	result := plus(2, 3, 4, 5)
-	fmt.Println(result)
 
-	//array
-	foods := [3]string{"potato", "pizza", "pasta"}
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) sayHello() {
+	fmt.Printf("Hello! My name is %s and I'm %d", p.name, p.age)
+}
+
+func main() {
+	// result := plus(2, 3, 4, 5)
+	// fmt.Println(result)
+
+	// //array
+	// foods := [3]string{"potato", "pizza", "pasta"}
 
 	// for _, food := range foods {
 	// 	fmt.Println(food)
 	// }
 
-	//위와 동일하지만 loop 방식으로
-	for i := 0; i < len(foods); i++ {
-		fmt.Println(foods[i])
-	}
+	// //위와 동일하지만 loop 방식으로
+	// for i := 0; i < len(foods); i++ {
+	// 	fmt.Println(foods[i])
+	// }
 
-	fmt.Println("\nslices ver.")
-	//slice <- array는 길이를 미리 명시해둬야 하기 때문에 블록체인 같이 길이를 예측할 수 없는 경우 slice 사용
-	dishes := []string{"potato", "pizza", "pasta"}
+	// fmt.Println("\nslices ver.")
+	// //slice <- array는 길이를 미리 명시해둬야 하기 때문에 블록체인 같이 길이를 예측할 수 없는 경우 slice 사용
+	// dishes := []string{"potato", "pizza", "pasta"}
 
-	for i := 0; i < len(dishes); i++ {
-		fmt.Println(dishes[i])
-	}
+	// for i := 0; i < len(dishes); i++ {
+	// 	fmt.Println(dishes[i])
+	// }
+
+	nico := person{"bom", 23}
+	nico.sayHello()
 }
