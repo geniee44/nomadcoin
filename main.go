01a885f58ca633fa8dfc2ad32ec53c1dc2f8b6c9
geniee44 <geeniee44@gmail.com>
2022-01-27 21:52:49 +0900

    lesson 7

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/geniee44/nomadcoin/blockchain"
-)
+import "github.com/nomadcoders/nomadcoin/cli"
 
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	cli.Start()
 }
